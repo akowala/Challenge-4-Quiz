df2807b8c2bd2ddf170fca0289a141f7032134dd
akowala <110314169+akowala@users.noreply.github.com>
2022-09-08 13:35:07 -0500

    wrote all the qeustions in questions.js

---
+++ Assets/js/questions.js
@@ -0,0 +1,40 @@
+const questions = [
+    {
+        question: "The Javascript file has an extension of:",
+        options: [".javascript", ".java", ".js", ".css"],
+        answer: ".js",
+    },
+    {
+        question: "Which statement can not be used to declare a variable in JavaScript?",
+        options: ["let", "var", "int", "const"],
+        answer: "int",
+    },
+    {
+        question: "Inside what HTML tag would you put JavaScript code?",
+        options: ["<js>", "<scripting>", "<script>", "<javascript>"],
+        answer: "<script>",
+    },
+    {
+        question:
+            "Where is the coorect place to insert Javascript on a web page?",
+        options: ["inside <body>", "inside <head>", "inside <head> and <body>", "none of these"],
+        answer: "inside <head> and <body>",
+    },
+    {
+        question:
+            "When we don't assign a value to a variable it will be...?",
+        options: ["null", "undefined", "NaN", "none of the above"],
+        answer: "undefined",
+    },
+    {
+        question: "To add an element to the end of an array you use:",
+        options: ["pop", "add", "push", "addToEnd"],
+        answer: "push",
+    },
+    {
+        question: "A string can be converted to an array using which method:",
+        options: ["split", "slice", "splice", "piece"],
+        answer: "split",
+    },
+    
+];
