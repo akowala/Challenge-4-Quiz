00c82b6176db222a147d20b8e5186512d85209b3
akowala <110314169+akowala@users.noreply.github.com>
2022-09-09 09:58:53 -0500

    constructed the save list for highscores

--- Assets/js/scoreslist.js
+++ Assets/js/scoreslist.js
@@ -1,3 +1,12 @@
 const highScores = document.querySelector("#high-scores-list");
 
-// figure out json
+let scoreslist =
+    JSON.parse(window.localStorage.getItem("scoreslist")) || [];
+console.log(scoreslist);
+
+for (let i = 0; i < scoreslist.length; i++) {
+    let scoreEl = document.createElement("li");
+    scoreEl.textContent = `User: ${scoreslist[i].initials} - Score: ${scoreslist[i].score}`;
+    highScores.appendChild(scoreEl);
+}
+
--- Assets/js/script.js
+++ Assets/js/script.js
@@ -25,11 +25,72 @@ function quizTime() {
     }
 }
 
+// Starts the quiz when the start button is clicked
 function beginQuiz() {
     start.setAttribute("class", "hidden");
     questionsSection.removeAttribute("class");
     countdown = setInterval(quizTime, 1000);
     timer.textContent = timeLeft;
+    // Posts the next question when the current one is answered
     postQuestion();
   }
   
+  // Posts the question to the page/
+  function postQuestion() {
+    let currentQ = questions[qIndex];
+    newQuestion.textContent = currentQ.question;
+    optionsList.innerHTML = "";
+  
+    // Populates the answer options underneath the question
+    for (let i = 0; i < currentQ.options.length; i++) {
+        let option = currentQ.options[i];
+        let userOption = document.createElement("button");
+        userOption.textContent = i + 1 + ": " + option;
+        userOption.setAttribute("value", option);
+        userOption.setAttribute("class", "option");
+  
+        // Adds an event listener to each option and checks if the user is right or wrong
+        userOption.addEventListener("click", (e) => {
+            if (e.target.value !== questions[qIndex].answer) {
+                timeLeft -= 10;
+                if (timeLeft <= 0) {
+                    timeLeft = 0;
+                }
+                timer.textContent = timeLeft;
+                grade.textContent = "wrong";
+            } else {
+                grade.textContent = "correct";
+            }
+  
+            // Displays if the user is right or wrong for two seconds
+            grade.setAttribute("class", "grade");
+            setTimeout(() => {
+                grade.setAttribute("class", "grade hidden");
+            }, 2000);
+  
+            // Used to move to the next question
+            qIndex++;
+  
+            // Checks the time and if the user is at the end of the quiz, then ends the quiz if true, otherwise it moves to the next question
+            if (timeLeft <= 0 || qIndex === questions.length) {
+                endQuiz();
+            } else {
+                postQuestion();
+            }
+        });
+        // Appends each option to the list of options
+        optionsList.appendChild(userOption);
+    }
+  }
+  
+  // Stops the timer, displays the final screen, and sets the current time to be the score
+  function endQuiz() {
+    clearInterval(countdown);
+    finalScreen.removeAttribute("class");
+    let score = document.querySelector("#score");
+    score.textContent = timeLeft;
+    questionsSection.setAttribute("class", "hidden");
+  }
+  
+  // listens for the start button to be clicked, then begins quiz
+  startBtn.onclick = beginQuiz;
